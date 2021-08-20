87eb9fa6aa4efdbb9b0c2e52eb3d4e02fabf8402
upwon <1244753906@qq.com>
2021-08-20 16:56:18 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -19,8 +19,7 @@ public class MainActivity extends AppCompatActivity {
     ImageView buttonUndo;
 
 
-    private ArrayList<Button> buttonArrayList = new ArrayList<>(Arrays.asList(button0, button1, button2, button3,
-            button4, button5, button6, button7, button8, button9));
+    private ArrayList<Button> buttonArrayList = new ArrayList<>( );
 
     private StringBuilder currentInputNum = new StringBuilder();
     private ArrayList<Integer> numsList = new ArrayList<Integer>();
@@ -56,6 +55,11 @@ public class MainActivity extends AppCompatActivity {
         buttonEqual = (Button) findViewById(R.id.button_equal);
         buttonQuit = (Button) findViewById(R.id.button_close);     // 退出应用
         buttonUndo = (ImageView) findViewById(R.id.imageView_Undo);  // 撤销输入
+
+
+        buttonArrayList.addAll (Arrays.asList(button0, button1, button2, button3,
+                button4, button5, button6, button7, button8, button9));
+
 
         // 清空
         buttonAC.setOnClickListener(new View.OnClickListener() {
@@ -105,22 +109,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // 数字 0
-        button0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                numberButtonClicked(view);
-            }
-        });
+//        // 数字 0
+//        button0.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                numberButtonClicked(view);
+//            }
+//        });
+//
+//        // 数字 1
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                numberButtonClicked(view);
+//            }
+//        });
 
-        // 数字 1
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                numberButtonClicked(view);
-            }
-        });
-
+        // 数字 0 - 9
         for (Button it : buttonArrayList
         ) {
             it.setOnClickListener(new View.OnClickListener() {
@@ -134,17 +139,47 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // 运算符 + - * / % 操作
     private void operatorButtonClicked(View view) {
+
     }
 
+    // 数字按键操作
+    private void numberButtonClicked(View view) {
+
+        if (view == null) {
+            throw new NullPointerException("null cannot be cast to non-null type android.widget.TextView");
+        } else {
+            //将view强制转化为 Button
+            Button btn = (Button) view;
+            currentInputNum.append(btn.getText());
+
+            if (isNumStart) {
+                // 输入一个新的数字，添加至数组中
+                numsList.add(Integer.valueOf(btn.getText().toString()));
+
+                // 更改状态 即 已经不是一个数字的开始了
+                isNumStart = false;
+
+            } else {
+//                numsList[numsList.size()-1]=Integer.parseInt(currentInputNum.toString()) ;
+                // 第一个参数为索引位置，第二个为要修改的值
+                numsList.set(numsList.size() - 1, Integer.parseInt(currentInputNum.toString()));
+
+            }
+
+        }
+
+
+    }
+
+    // 撤销
     private void undoButtonClicked(View view) {
     }
 
+    // 清空
     private void clearAllButtonClicked(View view) {
     }
 
-    // 数字按键
-    private void numberButtonClicked(View view) {
 
-    }
 }
